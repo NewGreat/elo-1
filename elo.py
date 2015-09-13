17406a1bd8aac9936d87a619a572a6ae2ee1f66c
Samuel Jackson <samueljackson@outlook.com>
2015-09-13 20:51:53 +0100

    Add existing work on module.
    This includes Elo class implementation and a test suite.

---
+++ elo.py
@@ -0,0 +1,106 @@
+import numpy as np
+import pandas as pd
+
+
+class Elo(object):
+
+    def __init__(self, teams, **kwargs):
+        self.set_parameters(**kwargs)
+        self.ratings = self._initalise_ratings(teams)
+
+    def set_parameters(self, mean_rating=1500, k=20, home_advantage=0):
+        if mean_rating < 0:
+            raise ValueError("Mean rating must be >= 0")
+        self._mean_rating = mean_rating
+
+        if k <= 0:
+            raise ValueError("K parameter must be > 0")
+        self._k = k
+
+        if home_advantage < 0:
+            raise ValueError("Home advantage must be > 0")
+        self._home_advantage = home_advantage
+
+    def _initalise_ratings(self, teams):
+        inital_values = np.empty(teams.size)
+        inital_values.fill(self._mean_rating)
+        return pd.Series(data=inital_values, index=teams)
+
+    def calculate_likelihood(self, rating_a, rating_b, mov_multiplier=1, bias=0):
+        """ Compute the expected score for team A given
+        team A's rating (r_a) and team B's rating (r_b)
+        """
+        if rating_a < 0 or rating_b < 0:
+            raise ValueError("Ratings must be positive")
+
+        diff = (rating_b - (rating_a + bias))
+        likelihood = 1.0 / (1.0 + 10**(diff / 400.0))
+        return likelihood
+
+    def _marign_of_victory_multiplier(self, rating_a, rating_b, mov):
+        return np.log(np.fabs(mov)+1) * (2.2/((rating_a-rating_b)*.001+2.2))
+
+    def update_rating(self, rating, score, likelihood, mov_multiplier=1):
+        """ Compute the new rating from team A given their old rating (r_a)
+        their actual score (s_a) and there expected score (e_a)
+        """
+        if rating < 0 or score < 0 or likelihood < 0:
+            raise ValueError("Parameters must be positive")
+        if likelihood > 1:
+            raise ValueError("likelihood must be between 0 < likelihood < 1")
+
+        rating = rating + self._k * mov_multiplier * (score - likelihood)
+        return int(rating)
+
+    def predict(self, game):
+        home_team, away_team = game
+
+        if home_team not in self.ratings:
+            raise KeyError('Team %s is not found in the ratings' % home_team)
+        if away_team not in self.ratings:
+            raise KeyError('Team %s is not found in the ratings' % away_team)
+
+        home_rating = self.ratings[home_team]
+        away_rating = self.ratings[away_team]
+
+        likelihood_home = self.calculate_likelihood(home_rating, away_rating,
+                                                    bias=self._home_advantage)
+        likelihood_away = self.calculate_likelihood(away_rating, home_rating,
+                                                    bias=-self._home_advantage)
+
+        winner = home_team if likelihood_home > likelihood_away else away_team
+        return pd.DataFrame([[home_team, home_rating, likelihood_home, away_team, away_rating, likelihood_away, winner]],
+                            columns=['Team A', 'Elo Score', 'Win %', 'Team B', 'Elo Score', 'Win %', 'Predicted Winner'])
+
+    def train(self, game):
+        home_team, home_pts, away_team, away_pts = game
+        home_rating = self.ratings[home_team]
+        away_rating = self.ratings[away_team]
+
+        home_score, away_score = self._calculate_game_points(home_pts, away_pts)
+        prediction = self.predict((home_team, away_team))
+
+        home_likelihood = prediction.ix[:, 2][0]
+        away_likelihood = prediction.ix[:, 5][0]
+
+        mov = (home_pts-away_pts)
+        mov_multiplier = self._marign_of_victory_multiplier(home_rating, away_rating, mov)
+        self.ratings[home_team] = self.update_rating(home_rating, home_score, home_likelihood, mov_multiplier)
+        self.ratings[away_team] = self.update_rating(away_rating, away_score, away_likelihood, mov_multiplier)
+
+    def _calculate_game_points(self, home_pts, away_pts):
+        if home_pts == away_pts:
+            # game was a draw, no gain for either team.
+            return 0.5, 0.5
+        elif home_pts > away_pts:
+            # home team beat away team
+            return 1, 0
+        elif home_pts < away_pts:
+            # away team beat home team
+            return 0, 1
+
+    def revert_ratings_to_mean(self, reversion_weight):
+        if reversion_weight < 0 or reversion_weight > 1:
+            raise ValueError('Reversion weight must be 0 < w < 1')
+        self.ratings = (reversion_weight * self.ratings) + ((1-reversion_weight) * self._mean_rating)
+        self.ratings = self.ratings.astype(int)
---
+++ test/elo_test.py
@@ -0,0 +1,169 @@
+import unittest
+import pandas as pd
+import numpy as np
+import numpy.testing as npt
+from nose.tools import *
+
+from elo import Elo
+
+
+class EloTest(unittest.TestCase):
+
+    def setUp(self):
+        self._teams = pd.Series(data=['TeamA', 'TeamB', 'TeamC', 'TeamD'])
+        self._MEAN_ELO_RATING = 1500
+        self._elo = Elo(self._teams, mean_rating=self._MEAN_ELO_RATING)
+
+    def test_initialise_ratings(self):
+        ratings = self._elo.ratings
+
+        expected = np.empty(self._teams.shape)
+        expected.fill(self._MEAN_ELO_RATING)
+
+        npt.assert_equal(ratings.values, expected,
+                         err_msg='All ratings should be equal on initalisation.')
+
+    def test_calculate_likelihood(self):
+        score = self._elo.calculate_likelihood(self._MEAN_ELO_RATING, self._MEAN_ELO_RATING, 0)
+        assert_equal(score, 0.5, 'Score should be 0.5.')
+
+        score = self._elo.calculate_likelihood(self._MEAN_ELO_RATING, 0, bias=100)
+        assert_almost_equal(score, 1, places=3, msg='Score should almost be 1')
+
+        score = self._elo.calculate_likelihood(0, self._MEAN_ELO_RATING, bias=100)
+        assert_almost_equal(score, 0, places=3, msg='Score should almost be 0')
+
+        score = self._elo.calculate_likelihood(0, self._MEAN_ELO_RATING, bias=100)
+        assert_almost_equal(score, 0, places=3, msg='Score should almost be 0')
+
+    def test_calculate_likelihood_with_home_advantage(self):
+        score = self._elo.calculate_likelihood(self._MEAN_ELO_RATING, 0, bias=100)
+        assert_almost_equal(score, 1, places=3, msg='Score should almost be 1')
+
+        score = self._elo.calculate_likelihood(0, self._MEAN_ELO_RATING, bias=-100)
+        assert_almost_equal(score, 0, places=3, msg='Score should almost be 0')
+
+        score = self._elo.calculate_likelihood(self._MEAN_ELO_RATING, 1, bias=100)
+        score_unbiased = self._elo.calculate_likelihood(self._MEAN_ELO_RATING, 1, 100, bias=-100)
+        assert_not_equal(score, score_unbiased, msg='Score should not exactly equal, but should be close.')
+
+        score = self._elo.calculate_likelihood(self._MEAN_ELO_RATING, self._MEAN_ELO_RATING, bias=100)
+        print score
+        assert_almost_equal(score, 0.640, places=3, msg='Score should almost be biased towards team A.')
+
+    def test_calculate_likelihood_invalid(self):
+        assert_raises(ValueError, self._elo.calculate_likelihood, self._MEAN_ELO_RATING, -1, bias=100)
+        assert_raises(ValueError, self._elo.calculate_likelihood, -1, self._MEAN_ELO_RATING, bias=100)
+        assert_raises(ValueError, self._elo.calculate_likelihood, -1, -1, bias=100)
+
+    def test_update_rating(self):
+        rating = self._elo.update_rating(self._MEAN_ELO_RATING, 1, 0.5)
+        assert_equal(rating, 1510, 'Rating should be increased by 10.')
+
+        rating = self._elo.update_rating(self._MEAN_ELO_RATING, 0, 0.5)
+        assert_equal(rating, 1490, 'Rating should be decreased by 10.')
+
+        rating = self._elo.update_rating(self._MEAN_ELO_RATING, 0.5, 0.25)
+        assert_equal(rating, 1505, 'Rating should increase by 5.')
+
+        rating = self._elo.update_rating(self._MEAN_ELO_RATING, 0.5, 0.75)
+        assert_equal(rating, 1495, 'Rating should decrease by 5.')
+
+        rating = self._elo.update_rating(self._MEAN_ELO_RATING, 1, 1.0)
+        assert_equal(rating, 1500, 'Rating should be not change, we expected a win.')
+
+        rating = self._elo.update_rating(self._MEAN_ELO_RATING, 0, 0.0)
+        assert_equal(rating, 1500, 'Rating should be not change, we expected a loss.')
+
+        rating = self._elo.update_rating(self._MEAN_ELO_RATING, 0.5, 0.5)
+        assert_equal(rating, 1500, 'Rating should not change, we expected a tie.')
+
+    def test_update_rating_invalid(self):
+        assert_raises(ValueError, self._elo.update_rating, -1, 1, 0.5)
+        assert_raises(ValueError, self._elo.update_rating, 1, -1, 0.5)
+        assert_raises(ValueError, self._elo.update_rating, 1, 1, -0.5)
+
+    def test_predict_equal_ratings(self):
+        game = ('TeamA', 'TeamB')
+        prediction = self._elo.predict(game)
+        team_a_likelihood = prediction.ix[:, 2][0]
+        team_b_likelihood = prediction.ix[:, 5][0]
+        assert_equal(team_a_likelihood, team_b_likelihood,
+                     'Teams with equal ratings should be equally likely')
+
+    def test_predict_unequal_ratings(self):
+        self._elo.ratings['TeamA'] = 1800
+        self._elo.ratings['TeamB'] = 1200
+
+        game = ('TeamA', 'TeamB')
+        prediction = self._elo.predict(game)
+        team_a_likelihood = prediction.ix[:, 2][0]
+        team_b_likelihood = prediction.ix[:, 5][0]
+
+        assert_almost_equal(team_a_likelihood, 0.969346569968,
+                            msg='Team A should be more likely to win')
+        assert_almost_equal(team_b_likelihood, 0.0306534300317,
+                            msg='Teams B should be less likely to win')
+        assert_almost_equal(team_a_likelihood+team_b_likelihood, 1, places=2,
+                     msg='Total probability should sum to 1.')
+
+    def test_predict_key_error(self):
+        assert_raises(KeyError, self._elo.predict, ('NotATeam', 'TeamB'))
+        assert_raises(KeyError, self._elo.predict, ('TeamA', 'NotATeam'))
+        assert_raises(KeyError, self._elo.predict, ('NotATeam', 'NotATeam'))
+
+    def test_train_unequal(self):
+        game = ('TeamA', 23, 'TeamB', 14)
+        self._elo.train(game)
+
+        assert_equal(self._elo.ratings['TeamA'], 1523.0, 'TeamA should of increased their rating.')
+        assert_equal(self._elo.ratings['TeamB'], 1476.0, 'TeamB should of decreased their rating.')
+
+    def test_train_equal(self):
+        game = ('TeamA', 10, 'TeamB', 10)
+        self._elo.train(game)
+
+        assert_equal(self._elo.ratings['TeamA'], self._elo.ratings['TeamB'],
+                     'Game was a draw. Ratings should remain the same.')
+
+    def test_train_with_home_advantage(self):
+        self._elo._home_advantage = 100
+
+        game = ('TeamA', 10, 'TeamB', 10)
+        self._elo.train(game)
+
+        assert_equal(self._elo.ratings['TeamA'], self._elo.ratings['TeamB'],
+                         'Home team advantage should cause a draw not to result in equal rankings.')
+
+        game = ('TeamA', 11, 'TeamB', 10)
+        self._elo.train(game)
+
+        assert_not_equal(self._elo.ratings['TeamA'], self._elo.ratings['TeamB'],
+                         'Home team advantage should cause a draw not to result in equal rankings.')
+        assert_almost_equal(self._elo.ratings['TeamA'], 1504.0,
+                            msg='Team A increases slightly.')
+        assert_almost_equal(self._elo.ratings['TeamB'], 1495.0,
+                            msg='Team B decreases slightly.')
+
+    def test_train_key_error(self):
+        assert_raises(KeyError, self._elo.train, ('NotATeam', 1, 'TeamB', 1))
+        assert_raises(KeyError, self._elo.train, ('TeamA', 1, 'NotATeam', 1))
+        assert_raises(KeyError, self._elo.train, ('NotATeam', 1, 'NotATeam', 1))
+
+    def test_revert_ratings_to_mean(self):
+        self._elo.ratings['TeamA'] = 1800
+        self._elo.ratings['TeamB'] = 1200
+
+        self._elo.revert_ratings_to_mean(0.25)
+        team_a = self._elo.ratings.ix[0]
+        team_b = self._elo.ratings.ix[1]
+
+        assert_equal(team_a, 1575.0, 'Rating should be reverted to within 25\% of the mean.')
+        assert_equal(team_b, 1425.0, 'Rating should be reverted to within 25\% of the mean.')
+
+    def test_revert_ratings_to_mean_invalid(self):
+        assert_raises(ValueError, self._elo.revert_ratings_to_mean, -0.75)
+
+
+if __name__ == "__main__":
+    unittest.main()
